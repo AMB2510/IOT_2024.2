808b69cbf1fc32b25086061bcd99e64c7cc78f9b
AMB2510 <anderson.barbosa8@fatec.sp.gov.br>
2024-11-13 09:55:02 -0300

    Create ANDERSON_Projeto.py

---
+++ ANDERSON_Projeto.py
@@ -0,0 +1,82 @@
+#Bibliotecas utilizadas
+from sense_hat import SenseHat
+import time
+import thingspeak
+
+# Item 2 -  Crie um código onde os valores de medição sejam apresentados na placa 
+sense = SenseHat()
+sense.clear()
+
+# Definição das cores
+BLUE   = (0, 0, 255)
+GREEN  = (0, 255, 0)
+RED    = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# Credenciais do canal ThingSpeak
+IOT_ID  = "2731766"
+IOT_KEY = "IM1WR9S1MW5YM8BZ"
+IOT_CHN = thingspeak.Channel(IOT_ID, IOT_KEY)
+
+# Função para obter os dados do SenseHAT
+def obter_dados_sensores():
+    temperatura = sense.get_temperature()
+    umidade     = sense.get_humidity()
+    pressao     = sense.get_pressure()
+
+    # Simulação do valor da qualidade do ar
+    qualidade_ar = 75
+
+    return temperatura, umidade, pressao, qualidade_ar
+
+# Função para publicar dados no ThingSpeak
+def publicar_thingspeak(temperatura, umidade, pressao, qualidade_ar):
+    payload = {'field1': temperatura, 'field2': umidade, 'field3': pressao, 'field4': qualidade_ar}
+    
+        if (IOT_CHN.update(payload)):
+            print("Dados enviados para ThingSpeak com sucesso!")
+        else:
+            print("Erro ao enviar dados para o ThingSpeak!")
+	print("-" * 55)
+	time.sleep(5)
+
+
+# Função para exibir a mensagem no SenseHat
+def exibir_mensagem(temperatura, umidade, pressao, qualidade_ar):
+    # Item 3 - Apresente o nome do seu projeto na cor azul no display do SenseHat
+    sense.show_message("ANDERSON_2161392313034", text_colour=BLUE)
+    time.sleep(1)
+    
+
+    # Item 4 - Informação na matriz de leds com as condições do tempo
+    # Alerta das condições climáticas
+    if temperatura <= 25 and 40 <= umidade < 70 and pressao > 1014 and qualidade_ar <= 40:
+        # Condição normal
+        cor_status = GREEN
+        status = "CONDICAO NORMAL"
+
+    elif (25 <= temperatura < 32 or 40 < umidade < 12 or 1014 <= pressao < 1025 or 40 < qualidade_ar < 80):
+        # Alerta de Atenção
+        cor_status = YELLOW
+        status = "ATENCAO"
+
+    else:
+        # Alerta de Perigo
+        cor_status = RED
+        status = "PERIGO"
+    
+    # Exibe as condições
+    sense.show_message(status, text_colour = cor_status)
+    sense.show_message(f"\nT: {temperatura:.1f}°C\nU: {umidade:.1f}%\nP: {pressao:.1f}hPa\nQA: {qualidade_ar}", text_colour = cor_status)
+    
+# Loop principal
+while True:
+    temperatura, umidade, pressao, qualidade_ar = obter_dados_sensores()
+    print(f"Temperatura: {temperatura:.1f}°C, Umidade: {umidade:.1f}%, Pressao: {pressao:.1f} hPa, Qualidade do Ar: {qualidade_ar}")
+
+    # Publicação dos dados no ThingSpeak
+    publicar_thingspeak(temperatura, umidade, pressao, qualidade_ar)
+
+    # Exibição das condições no SenseHat
+    exibir_mensagem(temperatura, umidade, pressao, qualidade_ar)
+    time.sleep(5)
